7727e666a35981a3ac929607828d32cc6e79723d
nevenacuturilov <cuturilovnevena@gmail.com>
2019-04-07 21:39:30 +0200

    Odradjene neke osnovne operacije nad frejmom.

--- program.py
+++ program.py
@@ -1,38 +1,40 @@
 import cv2
 import os
+import numpy as np
 
 # lista sa imenima svih 10 videa kroz koju moze da se prodje da bi se svaki video ucitao i analizirao
 
-folderName = 'videos/'
+folder_name = 'videos/'
 
-videoPaths = []
+video_paths = []
 
-for videoName in os.listdir(folderName):
-    videoPath = os.path.join(folderName, videoName)
-    print(videoPath)
-    videoPaths.append(videoPath)
+for video_name in os.listdir(folder_name):
+    video_path = os.path.join(folder_name, video_name)
+    print(video_path)
+    video_paths.append(video_path)
 
-print("Broj video snimaka: ", len(videoPaths))
-
+print("Broj video snimaka: ", len(video_paths))
 
 # za sada se radi sa samo jednim videom
+
 name = "videos/video6.mp4"
 
 # ucitavanje videa
-frame_num = 0  # index frejma
-cap = cv2.VideoCapture(name)  # open video
-print(cap.isOpened())  # true ako je otvoren - **da se doda za GRESKU
-cap.set(1, frame_num)  # napravljen property za indeksiranje frejmova
 
+frame_id = 0  # index frejma
+capture = cv2.VideoCapture(name)  # open video
+print(capture.isOpened())  # true ako je otvoren - ** da se doda za GRESKU
+capture.set(1, frame_id)  # napravljen property za indeksiranje frejmova
 
 # analiza videa frejm po frejm
+
 while True:
-    frame_num += 1
-    ret_val, frame = cap.read()  # ret je true or false, cita frejm po frejm
+    frame_id += 1
+    return_value, frame = capture.read()  # ret je true or false, cita frejm po frejm
 
     # ako frejm nije zahvacen - javi se greska kad nema ovog dela koda zato sto se ne uhvati bas svaki
     # frejm pa onda ovo ispod nema s cim da radi
-    if not ret_val:
+    if not return_value:
         break
 
     '''if ret_val == 'false':
@@ -40,4 +42,14 @@ while True:
 
     # dalje se sa frejmom radi kao sa bilo kojom drugom slikom
 
-cap.release()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_gray = cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2GRAY)
+
+    frame_blur = cv2.GaussianBlur(frame_gray, (0, 0), 3)
+    cv2.addWeighted(frame_gray, 1.5, frame_blur, -0.5, 0)
+
+    frame_crop = frame_gray[90:460, 170:530]  # brojevi su rucno podeseni - **mozda je moglo i drugacije
+
+capture.release()
+
+# **upis
