95e6f7193de5549554a1251caf0af580e82e7241
nevenacuturilov <cuturilovnevena@gmail.com>
2019-04-07 13:33:18 +0200

    Uradjen prolazak kroz sve video zapise.

--- program.py
+++ program.py
@@ -1,6 +1,20 @@
 import cv2
+import os
 
-# **lista sa imenima svih 10 videa kroz koju moze da se prodje da bi se svaki video ucitao i analizirao
+# lista sa imenima svih 10 videa kroz koju moze da se prodje da bi se svaki video ucitao i analizirao
+
+folderName = 'videos/'
+
+videoPaths = []
+
+for videoName in os.listdir(folderName):
+    videoPath = os.path.join(folderName, videoName)
+    print(videoPath)
+    videoPaths.append(videoPath)
+
+print("Broj video snimaka: ", len(videoPaths))
+
+
 # za sada se radi sa samo jednim videom
 name = "videos/video6.mp4"
 
@@ -10,18 +24,20 @@ cap = cv2.VideoCapture(name)  # open video
 print(cap.isOpened())  # true ako je otvoren - **da se doda za GRESKU
 cap.set(1, frame_num)  # napravljen property za indeksiranje frejmova
 
+
 # analiza videa frejm po frejm
 while True:
-    print(frame_num)
     frame_num += 1
     ret_val, frame = cap.read()  # ret je true or false, cita frejm po frejm
-    
+
     # ako frejm nije zahvacen - javi se greska kad nema ovog dela koda zato sto se ne uhvati bas svaki
     # frejm pa onda ovo ispod nema s cim da radi
     if not ret_val:
         break
 
     '''if ret_val == 'false':
-        break'''  # ne radi - zasto?
+        break'''  # ne radi - zasto? ** probati sa !=
+
+    # dalje se sa frejmom radi kao sa bilo kojom drugom slikom
 
 cap.release()
